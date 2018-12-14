3c6a463abc6e43debb17bc2cbb5f044606bf0b11
Jonathan Fraimorice <yonifra@gmail.com>
2018-12-14 23:22:39 +0200

    Added fonts

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -7,6 +7,10 @@ class Home extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       verticalDirection: VerticalDirection.down,
       children: <Widget>[
+        Text(sayHello(), style: TextStyle(
+          fontSize: 24.0,
+          fontFamily: 'Manrope'
+        ),),
         MaterialButton(child: Text("בניין 47")),
         RaisedButton(
           child: Text("בניין 48"),
@@ -17,5 +21,24 @@ class Home extends StatelessWidget {
         MaterialButton(child: Text("בניין 47")),
       ],
     );
+  }
+
+  String sayHello() {
+    String hello;
+    DateTime now = new DateTime.now();
+    int hour = now.hour;
+    int minute = now.minute;
+
+    if (hour < 12) {
+      hello = "Good morning";
+    } else if (hour < 18) {
+      hello = "Good afternoon";
+    } else {
+      hello = "Good evening";
+    }
+
+    String minutes = (minute < 10) ? "0" + minute.toString() : minute.toString();
+
+    return "It's now " + hour.toString() + ":" + minutes + "\n" + hello;
   }
 }
